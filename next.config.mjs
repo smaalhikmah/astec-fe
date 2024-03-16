339fabc53fb63876f479c3f859baece4428c1d7d
Rendi <rendicrew96@gmail.com>
2024-03-17 02:20:05 +0700

    fix:image url

--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,7 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'api.smaalhikmah.com',
         port: '',
-        pathname: '/image/',
+        pathname: '/image/**',
       },
     ],
   }
